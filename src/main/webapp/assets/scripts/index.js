d6ed7e447f2dd90ab6dc694a29a1315856b25b11
santiago tovar <santiagoriscanevo@outlook.com>
2020-08-08 20:15:38 -0500

    working about me page

--- src/main/webapp/assets/scripts/index.js
+++ src/main/webapp/assets/scripts/index.js
@@ -2,22 +2,15 @@ import Animated from "./components/animated.js"
 
 //get datas
 const headerText = document.querySelector('.header_left_text')
-
 const buttonHeader = document.querySelector('.header_button')
-
 const aboutText = document.querySelector('.about_text')
-
 const aboutContact = document.querySelectorAll('.contact_button')
-
 // => quote 
 const quote = document.getElementById("quote")
-
 // => experience
 const experience = document.querySelectorAll(".experience_card")
-
 //call Object
 window.animations = new Animated(headerText,buttonHeader,aboutText,aboutContact,quote,experience)
-
 //execute
 window.addEventListener("DOMContentLoaded", function(){
     animations.startLeft()
@@ -25,7 +18,6 @@ window.addEventListener("DOMContentLoaded", function(){
     animations.aboutUp()
     animations.aboutContact()
 })
-
 window.addEventListener("scroll", () => {
     let coords = quote.getBoundingClientRect()
 
